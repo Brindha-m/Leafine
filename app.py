8c03f8a29b26dc3c1e3a83d24acdcd8be0211af3
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-03-17 00:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 import time
 from datetime import datetime
 import utilsv8
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
